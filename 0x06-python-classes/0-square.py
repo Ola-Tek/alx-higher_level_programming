2e8264d358e69cad72a942ecabc26c159511fda1
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-12-19 10:33:00 -0800

    classes and attributes with corrections

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """square module,"""
 
+
 class Square:
     """Defines a square"""
+
     pass
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """square module"""
 
+
 class Square:
     """Defibe a square"""
 
