862293538b7d18aca3fead99a34e12beef3bbb46
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-12-19 09:37:46 -0800

    a squre with an attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square module"""
+
+class square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """constructor
+
+        Args:
+            size: length of a side of a aquare.
+            """
+        self.__size = size
