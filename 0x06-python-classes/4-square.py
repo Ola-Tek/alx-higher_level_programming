0504471422b738660e0d79501c4b3476ed0cfbbb
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-12-19 11:56:17 -0800

    a size class that defines a square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """Defines a swuare."""
+
+    def __init__(self, size=0):
+        """constructor
+
+        Arguments:
+            size: length of a side of the square
+            """
+        self.size = size
+
+    @property
+    def size(self):
+        """property for the length of the square.
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+                """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Area of this square
+
+        Returns:
+            The size squared
+        """
+
+        return self.size ** 2
