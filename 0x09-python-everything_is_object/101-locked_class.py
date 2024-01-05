7f2ed354abe50e51551b41f6ad07caa3bfdafc87
Obisesan Olabode <Obisesanolabode10@gmail.com>
2024-01-05 15:10:03 -0800

    advanced tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+
+class lockedclass
+"""
+    prevent the user from instantiating new locked class attribute for anything but attrivutes called firstname.
+    """
+
+    __slots__ = ["first_name"]
